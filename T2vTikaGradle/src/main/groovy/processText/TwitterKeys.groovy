15ab0c0dcda31395296753d81f4e133088e9a917
txt2vz <lauriehirsch@gmail.com>
2016-07-15 11:43:41 +0100

    k

---
+++ T2vTikaGradle/src/main/groovy/processText/TwitterKeys.groovy
@@ -0,0 +1,8 @@
+package processText
+abstract class TwitterKeys extends Script {
+	
+	def consumerKey = "key"
+	def consumerSecret = "key"
+	def accessToken = "key"
+	def accessTokenSecret = "key"
+}

09b69a4c04ad42b70a3149794f7904eb70bd47ba
txt2vz <lauriehirsch@gmail.com>
2016-08-04 14:11:05 +0100

    new spock test

--- T2vTikaGradle/src/test/groovy/wordLinks/WordLinksSpec.groovy
+++ T2vTikaGradle/src/test/groovy/wordLinks/WordLinksSpec.groovy
@@ -15,9 +15,9 @@ class WordLinksSpec extends spock.lang.Specification {
 		def json = slurper.parseText(jsonText)
 
 		then:
-		jsonText =='''{"name":"one1","cooc":1.0,"children":[{"name":"two2","cooc":1.0,"children":[{"name":"three3"}]}]}'''
-		json.name =="one1"
-		json.children[0].name =="two2"
+		jsonText == '''{"name":"one1","cooc":1.0,"children":[{"name":"two2","cooc":1.0,"children":[{"name":"three3"}]}]}'''
+		json.name == "one1"
+		json.children[0].name == "two2"
 	}
 
 	def "Muhammad Ali text wordlinks test"(){
